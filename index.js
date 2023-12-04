cfe8cf19c255f9042ba647b6564adcd475055a66
Radoje <megasrbin1@gmail.com>
2023-12-04 11:25:04 +0100

    restore default values for scale and trasnform-origin

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-let scale = 1;
+let scale = 1
 const el = document.documentElement
 
 el.addEventListener("wheel", function(e) {
-	const {x, y, deltaY, shiftKey} = e;
+	const {x, y, deltaY, shiftKey} = e
 	
-	scale += deltaY * -0.002;
-	scale = Math.min(Math.max(1, scale), 4);
+	scale += deltaY * -0.004
+	scale = Math.min(Math.max(1, scale), 4)
 	if(shiftKey && scale >= 1) {
 		e.stopPropagation()
 		e.preventDefault()
 
-		const rect = el.getBoundingClientRect();
-		const currentScrollLeft = el.scrollLeft;
-		const currentScrollTop = el.scrollTop;
+		const rect = el.getBoundingClientRect()
+		const currentScrollLeft = el.scrollLeft
+		const currentScrollTop = el.scrollTop
 
-		const newScaledWidth = el.offsetWidth * scale;
-		const newScaledHeight = el.offsetHeight * scale;
+		const newScaledWidth = el.offsetWidth * scale
+		const newScaledHeight = el.offsetHeight * scale
 
-		const mouseOffsetX = (x - rect.left) / rect.width;
-		const mouseOffsetY = (y - rect.top) / rect.height;
+		const mouseOffsetX = (x - rect.left) / rect.width
+		const mouseOffsetY = (y - rect.top) / rect.height
 
-		const newScrollLeft = (mouseOffsetX * newScaledWidth) - (x - rect.left) + currentScrollLeft;
-		const newScrollTop = (mouseOffsetY * newScaledHeight) - (y - rect.top) + currentScrollTop;
+		const newScrollLeft = (mouseOffsetX * newScaledWidth) - (x - rect.left) + currentScrollLeft
+		const newScrollTop = (mouseOffsetY * newScaledHeight) - (y - rect.top) + currentScrollTop
 		
 		el.style.scale = scale
 		el.style.transformOrigin = "0 0"
 
-		el.scrollLeft = newScrollLeft;
-		el.scrollTop = newScrollTop;
-
+		el.scrollLeft = newScrollLeft
+		el.scrollTop = newScrollTop
+	}
+	if(scale === 1) {
+		el.style.scale = "initial"
+		el.style.transformOrigin = "initial"
 	}
 }, {passive: false})
 
