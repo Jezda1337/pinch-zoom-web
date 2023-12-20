9398a29e920146059f51363c3348243c89bc6845
Radoje <megasrbin1@gmail.com>
2023-12-20 19:59:27 +0100

    fix bug with reseting the scale on scroll

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
-let scale = 1
-const el = document.documentElement
+let scale = 1;
+const el = document.documentElement;
 
-el.addEventListener("wheel", function(e) {
-	const {x, y, deltaY, shiftKey} = e
-	
-	scale += deltaY * -0.004
-	scale = Math.min(Math.max(1, scale), 4)
-	if(shiftKey && scale >= 1) {
-		e.stopPropagation()
-		e.preventDefault()
+el.addEventListener(
+  "wheel",
+  function (e) {
+    const { x, y, deltaY, shiftKey } = e;
 
-		const rect = el.getBoundingClientRect()
-		const currentScrollLeft = el.scrollLeft
-		const currentScrollTop = el.scrollTop
+    if (shiftKey && scale >= 1) {
+      e.stopPropagation();
+      e.preventDefault();
 
-		const newScaledWidth = el.offsetWidth * scale
-		const newScaledHeight = el.offsetHeight * scale
+      scale += deltaY * -0.004;
+      scale = Math.min(Math.max(1, scale), 4);
 
-		const mouseOffsetX = (x - rect.left) / rect.width
-		const mouseOffsetY = (y - rect.top) / rect.height
+      const rect = el.getBoundingClientRect();
+      const currentScrollLeft = el.scrollLeft;
+      const currentScrollTop = el.scrollTop;
 
-		const newScrollLeft = (mouseOffsetX * newScaledWidth) - (x - rect.left) + currentScrollLeft
-		const newScrollTop = (mouseOffsetY * newScaledHeight) - (y - rect.top) + currentScrollTop
-		
-		el.style.scale = scale
-		el.style.transformOrigin = "0 0"
+      const newScaledWidth = el.offsetWidth * scale;
+      const newScaledHeight = el.offsetHeight * scale;
 
-		el.scrollLeft = newScrollLeft
-		el.scrollTop = newScrollTop
-	}
-	if(scale === 1) {
-		el.style.scale = "initial"
-		el.style.transformOrigin = "initial"
-	}
-}, {passive: false})
+      const mouseOffsetX = (x - rect.left) / rect.width;
+      const mouseOffsetY = (y - rect.top) / rect.height;
 
+      const newScrollLeft =
+        mouseOffsetX * newScaledWidth - (x - rect.left) + currentScrollLeft;
+      const newScrollTop =
+        mouseOffsetY * newScaledHeight - (y - rect.top) + currentScrollTop;
+
+      el.style.scale = scale;
+      el.style.transformOrigin = "0 0";
+
+      el.scrollLeft = newScrollLeft;
+      el.scrollTop = newScrollTop;
+    }
+    if (scale === 1) {
+      el.style.scale = "initial";
+      el.style.transformOrigin = "initial";
+    }
+  },
+  { passive: false },
+);
