2628008551ae134357521df51ef8d973768a89a4
Radoje <megasrbin1@gmail.com>
2024-03-05 19:08:31 +0100

    tiny scope update

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-window.addEventListener("load", () => {
+window.addEventListener("load", function () {
   let scale = 1;
-  const el = document.documentElement;
   let newScrollLeft, newScrollTop;
-
-  const allElements = document.querySelectorAll("*");
+  const el = document.documentElement;
 
   const styling = {
     scale: "initial",
@@ -19,57 +17,62 @@ window.addEventListener("load", () => {
   el.style.scrollBehavior = styling.scrollBehavior;
   el.style.backfaceVisibility = styling.backfaceVisibility;
 
-  el.addEventListener(
-    "wheel",
-    function (e) {
-      const { x, y, deltaY, shiftKey } = e;
+  function zoom(e) {
+    const { x, y, deltaY, shiftKey } = e;
 
-      if (shiftKey && scale >= 1) {
-        e.stopPropagation();
-        e.preventDefault();
+    if (shiftKey && scale >= 1) {
+      const allElements = document.querySelectorAll("*");
+      e.stopPropagation();
+      e.preventDefault();
 
-        scale += deltaY * -0.004;
-        scale = Math.min(Math.max(1, scale), 4);
+      scale += deltaY * -0.004;
+      scale = Math.min(Math.max(1, scale), 4);
 
-        const rect = el.getBoundingClientRect();
-        const currentScrollLeft = el.scrollLeft;
-        const currentScrollTop = el.scrollTop;
+      const rect = el.getBoundingClientRect();
+      const currentScrollLeft = el.scrollLeft;
+      const currentScrollTop = el.scrollTop;
 
-        const newScaledWidth = el.offsetWidth * scale;
-        const newScaledHeight = el.offsetHeight * scale;
+      const newScaledWidth = el.offsetWidth * scale;
+      const newScaledHeight = el.offsetHeight * scale;
 
-        const mouseOffsetX = (x - rect.left) / rect.width;
-        const mouseOffsetY = (y - rect.top) / rect.height;
+      const mouseOffsetX = (x - rect.left) / rect.width;
+      const mouseOffsetY = (y - rect.top) / rect.height;
 
-        newScrollLeft =
-          mouseOffsetX * newScaledWidth - (x - rect.left) + currentScrollLeft;
-        newScrollTop =
-          mouseOffsetY * newScaledHeight - (y - rect.top) + currentScrollTop;
+      newScrollLeft =
+        mouseOffsetX * newScaledWidth - (x - rect.left) + currentScrollLeft;
+      newScrollTop =
+        mouseOffsetY * newScaledHeight - (y - rect.top) + currentScrollTop;
 
-        el.style.scale = scale;
-        el.style.transformOrigin = "top left 0px";
-        el.style.scrollBehavior = "unset";
-        el.style.backfaceVisibility = "hidden";
+      el.style.scale = scale;
+      el.style.transformOrigin = "top left 0px";
+      el.style.scrollBehavior = "unset";
+      el.style.backfaceVisibility = "hidden";
 
-        el.scrollLeft = newScrollLeft;
-        el.scrollTop = newScrollTop;
-      }
-
-      if (scale === 1) {
-        el.style.scale = "initial";
-        el.style.transformOrigin = "initial";
-        el.style.scrollBehavior = "initial";
-      }
+      el.scrollLeft = newScrollLeft;
+      el.scrollTop = newScrollTop;
 
       // elements with sticky position act strange when scale is applyed so this is some workaroud to prevent breaking the layout
       allElements.forEach((element) => {
         const computedStyle = window.getComputedStyle(element);
         if (computedStyle.position === "sticky") {
           if (scale > 1) element.style.top = "unset";
-          else element.style.top = 0;
+          else element.style.removeProperty("top");
+        }
+        if (computedStyle.position === "fixed") {
+          if (scale > 1)
+            element.parentElement.style.transform = "translateZ(0)";
+          else element.parentElement.style.removeProperty("transform");
         }
       });
-    },
-    { passive: false },
-  );
+    }
+
+    if (scale === 1) {
+      el.style.scale = "initial";
+      el.style.transformOrigin = "initial";
+      el.style.scrollBehavior = "initial";
+    }
+  }
+
+  window.addEventListener("wheel", zoom, { passive: false });
+  window.addEventListener("hashchange", zoom, { passive: false });
 });
