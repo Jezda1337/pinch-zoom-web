c6e0e309c5ebced87c1c21ef9d2a218f2471703e
Radoje <megasrbin1@gmail.com>
2024-03-05 14:09:31 +0100

    workaroud for sticky elements

--- index.js
+++ index.js
@@ -1,72 +1,75 @@
-let scale = 1;
-const el = document.documentElement;
-let newScrollLeft, newScrollTop;
+window.addEventListener("load", () => {
+  let scale = 1;
+  const el = document.documentElement;
+  let newScrollLeft, newScrollTop;
 
-const styling = {
-  scale: "initial",
-  transition: "initial",
-  transformOrigin: "0 0 0",
-  scrollBehavior: "unset",
-  backfaceVisibility: "hidden",
-};
+  const allElements = document.querySelectorAll("*");
 
-el.style.scale = styling.scale;
-el.style.transition = styling.transition;
-el.style.transformOrigin = styling.transformOrigin;
-el.style.scrollBehavior = styling.scrollBehavior;
-el.style.backfaceVisibility = styling.backfaceVisibility;
+  const styling = {
+    scale: "initial",
+    transition: "initial",
+    transformOrigin: "0 0 0",
+    scrollBehavior: "unset",
+    backfaceVisibility: "hidden",
+  };
 
-el.addEventListener(
-  "wheel",
-  function (e) {
-    const { x, y, deltaY, shiftKey } = e;
+  el.style.scale = styling.scale;
+  el.style.transition = styling.transition;
+  el.style.transformOrigin = styling.transformOrigin;
+  el.style.scrollBehavior = styling.scrollBehavior;
+  el.style.backfaceVisibility = styling.backfaceVisibility;
 
-    if (shiftKey && scale >= 1) {
-      e.stopPropagation();
-      e.preventDefault();
+  el.addEventListener(
+    "wheel",
+    function (e) {
+      const { x, y, deltaY, shiftKey } = e;
 
-      scale += deltaY * -0.004;
-      scale = Math.min(Math.max(1, scale), 4);
+      if (shiftKey && scale >= 1) {
+        e.stopPropagation();
+        e.preventDefault();
 
-      const rect = el.getBoundingClientRect();
-      const currentScrollLeft = el.scrollLeft;
-      const currentScrollTop = el.scrollTop;
+        scale += deltaY * -0.004;
+        scale = Math.min(Math.max(1, scale), 4);
 
-      const newScaledWidth = el.offsetWidth * scale;
-      const newScaledHeight = el.offsetHeight * scale;
+        const rect = el.getBoundingClientRect();
+        const currentScrollLeft = el.scrollLeft;
+        const currentScrollTop = el.scrollTop;
 
-      const mouseOffsetX = (x - rect.left) / rect.width;
-      const mouseOffsetY = (y - rect.top) / rect.height;
+        const newScaledWidth = el.offsetWidth * scale;
+        const newScaledHeight = el.offsetHeight * scale;
 
-      newScrollLeft =
-        mouseOffsetX * newScaledWidth - (x - rect.left) + currentScrollLeft;
-      newScrollTop =
-        mouseOffsetY * newScaledHeight - (y - rect.top) + currentScrollTop;
+        const mouseOffsetX = (x - rect.left) / rect.width;
+        const mouseOffsetY = (y - rect.top) / rect.height;
 
-      el.style.scale = scale;
-      // el.style.transition = "all 250ms linear";
-      // el.style.transformOrigin = `${x}px ${y}px`;
-      el.style.transformOrigin = "top left 0px";
-      el.style.scrollBehavior = "unset";
-      el.style.backfaceVisibility = "hidden";
+        newScrollLeft =
+          mouseOffsetX * newScaledWidth - (x - rect.left) + currentScrollLeft;
+        newScrollTop =
+          mouseOffsetY * newScaledHeight - (y - rect.top) + currentScrollTop;
 
-      el.scrollLeft = newScrollLeft;
-      el.scrollTop = newScrollTop;
-    }
-    if (scale === 1) {
-      el.style.scale = "initial";
-      el.style.transformOrigin = "initial";
-      el.style.scrollBehavior = "initial";
-    }
-  },
-  { passive: false },
-);
+        el.style.scale = scale;
+        el.style.transformOrigin = "top left 0px";
+        el.style.scrollBehavior = "unset";
+        el.style.backfaceVisibility = "hidden";
 
-// el.addEventListener("transitionend", function (e) {
-//   e.stopPropagation();
-//   e.preventDefault();
+        el.scrollLeft = newScrollLeft;
+        el.scrollTop = newScrollTop;
+      }
 
-// el.style.transformOrigin = "top left 0px";
-// el.scrollLeft = newScrollLeft;
-// el.scrollTop = newScrollTop;
-// });
+      if (scale === 1) {
+        el.style.scale = "initial";
+        el.style.transformOrigin = "initial";
+        el.style.scrollBehavior = "initial";
+      }
+
+      // elements with sticky position act strange when scale is applyed so this is some workaroud to prevent breaking the layout
+      allElements.forEach((element) => {
+        const computedStyle = window.getComputedStyle(element);
+        if (computedStyle.position === "sticky") {
+          if (scale > 1) element.style.top = "unset";
+          else element.style.top = 0;
+        }
+      });
+    },
+    { passive: false },
+  );
+});
